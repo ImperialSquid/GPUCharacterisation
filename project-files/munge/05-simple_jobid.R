e142708107fb6f5bcd7f127d3b3073d91e1209ff
Ben Jaeger <benjaeger99@gmail.com>
2024-01-07 03:28:30 +0000

    Update clean up - keep task_x_y, discard jobId col

--- project-files/munge/05-simple_jobid.R
+++ project-files/munge/05-simple_jobid.R
@@ -3,3 +3,6 @@
 checkps = checkps %>%
   mutate(level = gsub("(^1024-lvl)|(.{37}$)", "", jobId)) %>%
   select(-jobId)
+
+task_x_y = task_x_y %>% 
+  select(-jobId)
--- project-files/munge/99-cleanup.R
+++ project-files/munge/99-cleanup.R
@@ -1,3 +1,3 @@
 # Clean up unneeded original dataset and imtermediatary files for cleannes
 
-rm(checkps, gpu, task_x_y)
+rm(checkps, gpu)
